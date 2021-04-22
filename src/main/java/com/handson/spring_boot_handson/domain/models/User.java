3245696f32a26c1ad00fb30ad4217c420465cf15
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-04-22 21:24:34 +0900

    add user entity

--- src/main/java/com/handson/spring_boot_handson/domain/models/User.java
+++ src/main/java/com/handson/spring_boot_handson/domain/models/User.java
@@ -1,8 +1,22 @@
 package com.handson.spring_boot_handson.domain.models;
 
 import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "users")
 @Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
     private String name;
 }
